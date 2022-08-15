ddd232d4ed02d5cd95934946e2746e8c4121d72a
pancax <dakotasoles@gmail.com>
2022-08-15 10:16:15 -0500

    scriptrunner

--- ViolationDeltaDebugger/src/test/resources/projects/othertest_projects/SimpleTest3/src/main/java/MainSimpleTest3.java
+++ ViolationDeltaDebugger/src/test/resources/projects/othertest_projects/SimpleTest3/src/main/java/MainSimpleTest3.java
@@ -4,9 +4,7 @@ public class MainSimpleTest3 {
 
         System.out.println("Lets do some math!");
 
-        Calculator c = new Calculator();
         BrokenCalculator bc = new BrokenCalculator();
-        System.out.println(c.addition(1,1));
         System.out.println(bc.divide(1,1));
     }
 }
