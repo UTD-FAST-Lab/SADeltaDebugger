c4546d79c0811b5786c113379f0115b6fbe72ace
CrispyAlice2 <crispyalice2@gmail.com>
2023-06-29 19:27:22 -0500

    Add new test project for cg algorithm

---
+++ ViolationDeltaDebugger/src/test/resources/projects/othertest_projects/cgTest/src/main/java/MainFlatClassesTest.java
@@ -0,0 +1,13 @@
+public class MainFlatClassesTest {
+
+    public static void main(String[] args) {
+        cs.utd.soles.blob.Blob u = new cs.utd.soles.blob.Blob(7, "1");
+        u.divide();
+        Blob s = new Blob(8, 0);
+        s.divide();
+        doNothing(1, 1.0, "aa");
+    }
+
+    public static void doNothing(int i, double y, String strign) {
+    }
+}
